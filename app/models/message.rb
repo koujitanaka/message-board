02ce3cb7781983559ecc2afa4a00e7e7e715272a
koujitanaka <tanaka@energyfactory-japan.com>
2016-05-11 09:22:05 +0000

    kadai3

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,12 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  AGE_GE = 0
+  AGE_LE = 100
+  validates :age,  numericality: { only_integer: true, greater_than_or_equal_to: AGE_GE, less_than_or_equal_to: AGE_LE }, allow_blank: true, presence: false
+
+  def get_age
+    self.age ? self.age  : "年齢不詳"
+  end
 end
+  
